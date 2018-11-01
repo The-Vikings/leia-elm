6af2d555a5d77404339b413d66b06e471de7c117
Magnus Myrmo Osberg <magnusosb@gmail.com>
2018-11-01 11:59:20 +0900

    :sparkles: add view for testing material design and phoenix, remove template view

--- src/Model.elm
+++ src/Model.elm
@@ -1,16 +1,14 @@
 module Model exposing (Model, RemoteData(..), init, initialModel)
 
-import Contact.Commands
-import ContactList.Commands
-import Navigation
-import Routing exposing (Route(ListContactsRoute, ShowContactRoute))
 import Contact.Model exposing (Contact)
 import ContactList.Model exposing (ContactList)
 import Material
 import Material.Snackbar as Snackbar
 import Messages exposing (Msg(Mdl))
-
+import Navigation
 import Phoenix.Socket
+import Routing exposing (Route(ListContactsRoute, ShowContactRoute))
+
 
 type RemoteData e a
     = Failure e

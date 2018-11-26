71ba1ceace1dd2b597034501578f93a38992537a
Magnus Myrmo Osberg <magnusosb@gmail.com>
2018-11-27 05:43:01 +0900

    :fire: removing unused Chatroom-Update & Chatroom-Message. Skeleton remains for restructuring after EOSP

--- src/Components/Chatroom/Update.elm
+++ src/Components/Chatroom/Update.elm
@@ -1,21 +1,10 @@
 module Components.Chatroom.Update exposing (update)
 
-import Commands exposing (chatroomsApiUrl)
-import Components.Chatroom.Commands exposing (fetchAllChatrooms)
 import Components.Chatroom.Messages exposing (ChatroomMsg(..))
-import Components.Chatroom.Model exposing (Chatroom)
-import Components.Question.Model exposing (Question)
-import Http
-import Json.Decode exposing (Decoder, decodeString, field, list, map3, string, int)
-import Messages exposing (Msg)
-import Model exposing (Model, RemoteData(Failure, Success))
+import Components.Chatroom.Model exposing (..)
 
-
-update : ChatroomMsg -> Model -> ( Model, Cmd Msg )
+update : ChatroomMsg -> Chatroom -> ( Chatroom, Cmd ChatroomMsg )
 update msg model =
     case msg of
-        FetchChatroom (Ok response) ->
-            ( { model | chatroom = Success response }, Cmd.none )
-
-        FetchChatroom (Err _) ->
-            ( { model | chatroom = Failure "Chatroom not found" }, Cmd.none )
+        FetchChatroom -> 
+            ( model, Cmd.none )

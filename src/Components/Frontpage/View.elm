052b7bd11412767407e731d65723b67d404a907d
Magnus Myrmo Osberg <magnusosb@gmail.com>
2018-12-01 05:31:04 +0900

    :sparkles: add view, frontpage, chatroom

--- src/Components/Frontpage/View.elm
+++ src/Components/Frontpage/View.elm
@@ -1,11 +1,110 @@
-module Components.Frontpage.view exposing (view)
+module Components.Frontpage.View exposing (view)
 
-import Components.Frontpage.Model exposing (Frontpage)
+import Components.Chatroom.Model exposing (Chatroom)
+import Html exposing (Html, h3, text)
+import Material.Button as Button
+import Material.Card as Card
+import Material.Color as Color
+import Material.Elevation as Elevation
+import Material.Grid as Grid exposing (Device(..), cell, grid, size)
+import Material.Options as Options exposing (cs, css, when)
+import Material.Tooltip as Tooltip
+import Messages exposing (Msg(..))
 import Model exposing (Model)
-import Html exposing (Html, text)
-import Messages exposing (Msg)
+import RemoteData
+import Shared.View exposing (createErrorMessage, dynamic, viewError, white)
 
 
 view : Model -> Html Msg
-view model = 
-    text "Frontpage-component-view"
+view model =
+    grid [ Options.css "max-width" "1080px" ]
+        [ cell
+            [ size All 4
+            , size Tablet 8
+            , Options.css "align-items" "center"
+            ]
+            [ grid [ Grid.noSpacing ]
+                [ cell
+                    [ size All 4
+                    , size Tablet 4
+                    , size Desktop 12
+                    ]
+                    [ viewChatroomsOrError model
+                    , Options.div
+                        [ size All 1
+                        , css "height" "32px"
+                        ]
+                        []
+                    ]
+                ]
+            ]
+        ]
+
+
+viewChatroomsOrError : Model -> Html Msg
+viewChatroomsOrError model =
+    case model.allChatrooms of
+        RemoteData.NotAsked ->
+            text ""
+
+        RemoteData.Loading ->
+            h3 [] [ text "Loading..." ]
+
+        RemoteData.Success allChatrooms ->
+            viewChatroomCards allChatrooms model
+
+        RemoteData.Failure httpError ->
+            viewError (createErrorMessage httpError)
+
+
+viewChatroomCards : List Chatroom -> Model -> Html Msg
+viewChatroomCards chatroom model =
+    Html.table [] (List.map (chatroomCard model) chatroom)
+
+
+chatroomCard : Model -> Chatroom -> Html Msg
+chatroomCard model chatroom =
+    Card.view
+        [ Elevation.e2
+        , Tooltip.attach Mdl [ chatroom.id ]
+        , css "width" "100%"
+        ]
+        [ Card.title
+            [ css "min-height" "50px"
+            , css "padding" "0"
+
+            -- Clear default padding to encompass scrim
+            , Color.background <| Color.color Color.Blue Color.S900
+            ]
+            [ Card.head
+                [ white
+                , Options.scrim 0.75
+                , css "padding" "16px"
+
+                -- Restore default padding inside scrim
+                , css "width" "100%"
+                ]
+                [ text chatroom.name ]
+            ]
+        , Card.text []
+            [ text (toString chatroom.id) ]
+        , Card.actions
+            [ Card.border ]
+            [ Button.render Mdl
+                [ 1, 0 ]
+                model.mdl
+                [ Button.ripple
+                , Button.accent
+                , Options.onClick (SendHttpRequestChatroomWithQuestions (toString chatroom.id))
+                , Tooltip.attach Mdl [ chatroom.id ]
+                ]
+                [ text "Show Chatroom" ]
+            , Tooltip.render Mdl
+                [ chatroom.id ]
+                model.mdl
+                [ Tooltip.top
+                , Tooltip.large
+                ]
+                [ text "This button will load the chatroom" ]
+            ]
+        ]
--- src/View.elm
+++ src/View.elm
@@ -1,19 +1,14 @@
 module View exposing (view)
 
 import Components.Chatroom.View
+import Components.Frontpage.View
 import Components.PhoenixTest.View
-import Dict exposing (Dict)
 import Html exposing (Html, div, h1, header, section, text)
 import Html.Attributes exposing (class, href, style)
-import Html.Events exposing (onClick, onInput, onSubmit)
-import Material.Badge as Badge
-import Material.Button as Button
 import Material.Color as Color
 import Material.Layout as Layout
-import Material.List as List
 import Material.Options as Options exposing (cs, css, when)
 import Material.Scheme
-import Material.Snackbar as Snackbar
 import Messages exposing (Msg)
 import Model exposing (Model)
 
@@ -24,13 +19,14 @@ view model =
         Layout.render Messages.Mdl
             model.mdl
             [ Layout.fixedHeader
+            , Layout.waterfall True
             , Layout.onSelectTab Messages.SelectTab
             , Layout.selectedTab model.selectedTab
             , Options.css "display" "flex !important"
             , Options.css "flex-direction" "row"
             , Options.css "align-items" "center"
             ]
-            { header = [ h1 [ style [ ( "padding", "0rem" ) ] ] [ ] ]
+            { header = [ h1 [ style [ ( "padding", "0rem" ) ] ] [] ]
             , drawer = []
             , tabs = ( [ text "All chatrooms", text "Current Chatroom" ], [ Color.background (Color.color Color.Blue Color.S800) ] )
             , main =
@@ -39,33 +35,22 @@ view model =
             }
 
 
-
-{--
-viewHeader : Model -> Html Msg
-viewHeader model =
-    Layout.row
-        [ Color.background <| Color.color Color.Green Color.S100
-        , Color.text <| Color.color Color.Orange Color.S900
-        ]
-        [ Layout.title [] [ text "Chatroom message system demonstration" ]
-        , Layout.spacer
-        , Layout.navigation []
-            []
-        ]
---}
-
 viewBody : Model -> Html Msg
 viewBody model =
     case model.selectedTab of
         0 ->
-            phoenixtest model
+            frontpage model
 
         1 ->
-            view2 model
+            currentChatroom model
 
         _ ->
             text "404"
 
+
+frontpage : Model -> Html Msg
+frontpage model =
+    Components.Frontpage.View.view model
 
 
 phoenixtest : Model -> Html Msg
@@ -73,27 +58,6 @@ phoenixtest model =
     Components.PhoenixTest.View.view model
 
 
-page : Model -> Html Msg
-page model =
+currentChatroom : Model -> Html Msg
+currentChatroom model =
     Components.Chatroom.View.view model
-
-
-view2 : Model -> Html Msg
-view2 model =
-    Components.Chatroom.View.view2 model
-
-
-styles : String
-styles =
-    """
-   .demo-options .mdl-checkbox__box-outline {
-      border-color: rgba(255, 255, 255, 0.89);
-    }
-   .mdl-layout__drawer {
-      border: none !important;
-   }
-   .mdl-layout__drawer .mdl-navigation__link:hover {
-      background-color: #00BCD4 !important;
-      color: #37474F !important;
-    }
-   """
